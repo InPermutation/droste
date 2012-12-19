a52617a3ffce169272578c559e3f2f36b25c9a29
Jacob Krall <jacob@jkrall.net>
2012-12-19 18:31:54 -0500

    fix clamp bug

--- recursion.py
+++ recursion.py
@@ -15,7 +15,6 @@ def gif(sauce, frameCount, targetRect):
     def scale(i):
         factor = (math.pow(ratio[0], 1.0*i/frameCount),
                     math.pow(ratio[1], 1.0*i/frameCount))
-        print 'f=', factor, ';', i/frameCount
         return tuple(map(lambda x: x, 
                 (targetCenter[0] - targetCenter[0]/ratio[0]*factor[0],
                  targetCenter[1] - targetCenter[1]/ratio[1]*factor[1],
@@ -23,13 +22,11 @@ def gif(sauce, frameCount, targetRect):
                  targetSize[1] * factor[1])))
 
     for frame in range(frameCount):
-        print "Frame ", frame, fullSize
         dest = Image.new(sauce.mode, fullSize, 255*frame/frameCount)
         iterations = 1000 # maximum number of iterations allowed per frame
         i = frame + frameCount
         rectum = scale(i)
         while rectum[2] > 1 and rectum[3] > 1 and iterations > 0:
-            print i, rectum, iterations
             draw(sauce, rectum, dest)
             i -= frameCount
             rectum = scale(i)
@@ -40,15 +37,7 @@ def gif(sauce, frameCount, targetRect):
 
 def clamp(rectum, dest):
     left, top, w, h = int(rectum[0]), int(rectum[1]), int(rectum[2]), int(rectum[3])
-    left = max(left, 0)
-    top = max(top, 0)
-    right = min(left+w, dest.size[0])
-    bottom = min(top+h, dest.size[1])
-
-    size = (right-left, bottom-top)
-    box = (left, top, right, bottom)
-
-    return size, box
+    return (w,h), (left, top, left+w, top+h)
 
 def draw(sauce, target, dest):
     size, box = clamp(target, dest)
