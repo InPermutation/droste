b7614310f89a2e08025e33cf3503b84fc2b70506
Jacob Krall <jacob@jkrall.net>
2012-12-18 22:35:18 -0500

    render the image as a single-frame GIF

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request
-
+from images2gif import GifWriter
+import StringIO
+import PIL
 
 app = Flask(__name__)
 
@@ -11,7 +13,18 @@ def hello():
 @app.route('/', methods=['POST'])
 def upload():
     f = request.files['file']
-    return "got " +  f.content_type + ' - ' + str(f.content_length)
+
+    im = PIL.Image.open(f).convert("P")
+    
+    gw = GifWriter()
+    fp = StringIO.StringIO()
+
+    gw.writeGifToFile(fp, [im], [10], 0, [[0, 0]], [2])
+
+    bytes = fp.getvalue()
+    fp.close()
+
+    return bytes, None, {"Content-Type": "image/gif"}
 
 if  __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000
